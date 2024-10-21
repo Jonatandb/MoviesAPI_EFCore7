167d0dbeedc9fea9208e6054dbebc59864233a08
Jonatandb <jonatandb@gmail.com>
2024-10-21 00:07:34 -0300

    MaxLength to Nombre field on Genero entity added

--- Data/MoviesDbContext.cs
+++ Data/MoviesDbContext.cs
@@ -5,5 +5,11 @@ using MoviesAPI_EFCore7.Entities;
 
 public class MoviesDbContext(DbContextOptions<MoviesDbContext> options) : DbContext(options)
 {
+    protected override void OnModelCreating(ModelBuilder modelBuilder) {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<Genero>().HasKey(g => g.Id);
+        modelBuilder.Entity<Genero>().Property(g => g.Nombre).HasMaxLength(150);
+    }
+
     public DbSet<Genero> Generos => Set<Genero>();
 }
---
+++ Migrations/20241021024918_Setting_Genero_Nombre_MaxLength.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MoviesAPI_EFCore7.Migrations
+{
+    /// <inheritdoc />
+    public partial class Setting_Genero_Nombre_MaxLength : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Nombre",
+                table: "Generos",
+                type: "character varying(150)",
+                maxLength: 150,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Nombre",
+                table: "Generos",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(150)",
+                oldMaxLength: 150);
+        }
+    }
+}
