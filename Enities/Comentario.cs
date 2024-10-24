86cff91bddbf4113749575d18aa3e6a9e2f43353
Jonatandb <jonatandb@gmail.com>
2024-10-24 11:05:36 -0300

    Comentario entity added

---
+++ Enities/Comentario.cs
@@ -0,0 +1,9 @@
+﻿namespace MoviesAPI_EFCore7.Enities
+{
+    public class Comentario
+    {
+        public int Id { get; set; }
+        public string? Contenido { get; set; }
+        public bool Recomendar { get; set; }
+    }
+}
