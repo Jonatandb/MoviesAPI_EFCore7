79e38f62613f28f4c5fc463a9fbb7968313089fa
Jonatandb <jonatandb@gmail.com>
2024-10-24 00:55:15 -0300

    SQL queries file added

---
+++ Queries.sql
@@ -0,0 +1,7 @@
+SELECT * FROM public."__EFMigrationsHistory"
+ORDER BY "MigrationId" ASC
+
+select * from public."Generos";
+
+--DELETE FROM public."__EFMigrationsHistory"
+--WHERE "MigrationId" = '20241024031832_maxlengthupdated';
