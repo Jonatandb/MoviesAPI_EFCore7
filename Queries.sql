f24d966f126f6cd508e668b128e8b35942b9a215
Jonatandb <jonatandb@gmail.com>
2024-10-24 11:36:48 -0300

    Queries file updated

--- Queries.sql
+++ Queries.sql
@@ -1,7 +1,12 @@
 SELECT * FROM public."__EFMigrationsHistory"
-ORDER BY "MigrationId" ASC
+ORDER BY "MigrationId" ASC;
 
 select * from public."Generos";
+
+select * from public."Actores";
+
+select * from public."Comentarios";
+
 
 --DELETE FROM public."__EFMigrationsHistory"
 --WHERE "MigrationId" = '20241024031832_maxlengthupdated';
